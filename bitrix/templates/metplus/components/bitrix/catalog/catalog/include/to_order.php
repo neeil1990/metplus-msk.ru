49f872cc903594aba7527092a79daa38eac6f191
neeil1990 <neeil@mail.ru>
2021-09-20 14:31:49 +0300

    #125775 hot fix

--- bitrix/templates/metplus/components/bitrix/catalog/catalog/include/to_order.php
+++ bitrix/templates/metplus/components/bitrix/catalog/catalog/include/to_order.php
@@ -3,9 +3,7 @@ if(!$arParams['ID'])
     return false;
 ?>
 <div class="to_order-main">
-    <div class="to_order-text">
-        Данная продукция находится на складе в<br/>Воронеже.<br/>Только под заказ!
-    </div>
+    <div class="to_order-text" data-text="Данная продукция находится на складе в<br/>Воронеже.<br/>Только под заказ!"></div>
     <div class="to_order-btn">
         <a href="#" class="main-btn" id="inStockCat" data-cat_id="<?=$arParams['ID']?>">
             Я согласен, заказать <span class="glipf-cart"></span>
