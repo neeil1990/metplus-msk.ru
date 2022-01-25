723e00285ab5ffdd9525d3e053b4d696f0db6383
neeil1990 <neeil@mail.ru>
2022-01-25 16:30:51 +0300

    testing

--- test/index.php
+++ test/index.php
@@ -1,8 +1,20 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("test");
-?>
 
-server test
+include($_SERVER['DOCUMENT_ROOT'] . '/test/simple_html_dom.php');
+
+$html = new simple_html_dom();
+$html->load("<html><body><span>Hello!</span></body></html>");
+
+$span = $html->find('span');
+
+var_dump($span[0]->innertext);
+
+$content = $html->save();
+
+var_dump($content);
+
+?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
