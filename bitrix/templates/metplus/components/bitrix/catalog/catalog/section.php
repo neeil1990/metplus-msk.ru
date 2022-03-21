58ab66d04956906922459c3757ed69962074f804
neeil1990 <neeil@mail.ru>
2022-03-21 10:41:02 +0300

    #137711 additional

--- bitrix/templates/metplus/components/bitrix/catalog/catalog/section.php
+++ bitrix/templates/metplus/components/bitrix/catalog/catalog/section.php
@@ -382,6 +382,33 @@ if(isset($_COOKIE["in_stock_$arCurSection[ID]"]))
                         </div>
                     </div>
 
+                    <div class="row">
+                        <div class="col-md-12">
+                            <?$APPLICATION->IncludeComponent("prime:news.line", "accordion", Array(
+                                "ACTIVE_DATE_FORMAT" => "d.M.Y",	// Формат показа даты
+                                "CACHE_GROUPS" => "Y",	// Учитывать права доступа
+                                "CACHE_TIME" => "300",	// Время кеширования (сек.)
+                                "CACHE_TYPE" => "A",	// Тип кеширования
+                                "DETAIL_URL" => "",	// URL, ведущий на страницу с содержимым элемента раздела
+                                "FIELD_CODE" => array(	// Поля
+                                    0 => "DETAIL_TEXT",
+                                    1 => "",
+                                ),
+                                "ID" => $arCurSection['UF_ACCORDION'],
+                                "IBLOCKS" => array(	// Код информационного блока
+                                    0 => "35",
+                                ),
+                                "IBLOCK_TYPE" => "components",	// Тип информационного блока
+                                "NEWS_COUNT" => "100",	// Количество новостей на странице
+                                "SORT_BY1" => "SORT",	// Поле для первой сортировки новостей
+                                "SORT_BY2" => "ID",	// Поле для второй сортировки новостей
+                                "SORT_ORDER1" => "ASC",	// Направление для первой сортировки новостей
+                                "SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
+                            ),
+                                false
+                            );?>
+                        </div>
+                    </div>
                 </div>
             </div>
 
--- test/index.php
+++ test/index.php
@@ -3,33 +3,5 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("test");
 ?>
 
-<main class="main-content">
-    <div class="container">
-
-    <?$APPLICATION->IncludeComponent("bitrix:news.line", "accordion", Array(
-        "ACTIVE_DATE_FORMAT" => "d.M.Y",	// Формат показа даты
-        "CACHE_GROUPS" => "Y",	// Учитывать права доступа
-        "CACHE_TIME" => "300",	// Время кеширования (сек.)
-        "CACHE_TYPE" => "A",	// Тип кеширования
-        "DETAIL_URL" => "",	// URL, ведущий на страницу с содержимым элемента раздела
-        "FIELD_CODE" => array(	// Поля
-            0 => "DETAIL_TEXT",
-            1 => "",
-        ),
-        "IBLOCKS" => array(	// Код информационного блока
-            0 => "35",
-        ),
-        "IBLOCK_TYPE" => "components",	// Тип информационного блока
-        "NEWS_COUNT" => "100",	// Количество новостей на странице
-        "SORT_BY1" => "SORT",	// Поле для первой сортировки новостей
-        "SORT_BY2" => "ID",	// Поле для второй сортировки новостей
-        "SORT_ORDER1" => "ASC",	// Направление для первой сортировки новостей
-        "SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
-    ),
-        false
-    );?>
-
-    </div>
-</main>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
